ffb8226480277a5dc2c9e3af1161bf71a6163443
Alexander Shvaykin <skiline.alex@gmail.com>
2018-03-25 21:55:08 +0300

    first commit

---
+++ lib/bcp_internet_acquiring.rb
@@ -0,0 +1,5 @@
+require "bcp_internet_acquiring/version"
+
+module BcpInternetAcquiring
+  # Your code goes here...
+end
---
+++ spec/bcp_internet_acquiring_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe BcpInternetAcquiring do
+  it "has a version number" do
+    expect(BcpInternetAcquiring::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
